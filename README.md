08c2a9d6ef59013f0103d055e579972a80bff746
bartvanbenthem <bartvanbenthem@hotmail.com>
2022-07-05 14:26:35 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,95 @@
+# Introduction 
+cli client to execute Azure pipelines over REST API
+
+## Non required prereqs
+for structured formatting the json output install the jq utillity 
+```bash
+# use the Snap package manager
+sudo snap install jq
+# or use the apt package manager
+sudo apt install jq
+```
+
+## Set environment variables (required)
+```bash
+# Linux Bash
+export PAT='Azure DevOps personal access token'
+export ORGANIZATION='ORG_NAME'
+export PROJECT='PROJECT_NAME'
+export PIPELINE_ID='999999'
+```
+
+```powershell
+# Windows PowerShell
+$env:PAT = 'Azure DevOps personal access token'
+$env:ORGANIZATION = 'ORG_NAME'
+$env:ADOPROJECT = 'PROJECT_NAME'
+$env:ADOPIPELINEID = '999999'
+```
+
+## Build and Run
+```bash
+#clone repository and change dir
+git clone 'https://github.com/bartvanbenthem/ado-runsapi-client.git'
+cd ado-runsapi-client
+# build run-pipeline
+go build -o bin ./cmd/run-pipeline
+# build get azure pipeline ID based on pipeline name
+go build -o bin ./cmd/run-status
+
+
+# run-pipeline and print full output
+./bin/run-pipeline | jq .
+# or run-pipeline and print only the run ID
+./bin/run-pipeline --printid='true'
+
+# check pipeline status for completion
+./bin/run-status --runid='999999'
+
+# Oneliner for Starting a new run of the pipeline and keep track of the current state
+RUNID=$(./bin/run-pipeline \
+            --printid='true' \
+            --parameters="{\"param1\": \"myvalue-1\", \"param2\": \"golang rules\"}") \
+&& ./bin/run-status --runid=$RUNID
+
+```
+
+### CURL examples
+```bash
+## Test if authentication working (List projects in your organization)
+curl -s -u $USER:$PAT \
+"https://dev.azure.com/$ORGANIZATION/_apis/projects?api-version=6.0" | jq .
+
+# list all pipelines example
+curl -s --request GET \
+-u 'runs':$PAT \
+--header "Content-Type: application/json" \
+"https://dev.azure.com/$ORGANIZATION/$PROJECT/_apis/pipelines?api-version=7.1-preview.1" | jq .
+
+# run pipeline example
+curl -s --request POST \
+-u 'runs':$PAT \
+--header "Content-Type: application/json" \
+--data '{
+    "resources": {
+        "repositories": {
+            "self": {
+                "refName": "refs/heads/main"
+            }
+        }
+    },
+   "templateParameters": {
+        "param1": "myvalue1",
+        "param2": "myvaluex"
+    }
+}' \
+"https://dev.azure.com/$ORGANIZATION/$PROJECT/_apis/pipelines/$PIPELINE_ID/runs?api-version=6.0-preview.1" | jq .
+
+# get pipeline run status
+RUN_ID='999999'
+curl -s --request GET \
+-u 'runs':$PAT \
+--header "Content-Type: application/json" \
+"https://dev.azure.com/$ORGANIZATION/$PROJECT/_apis/pipelines/$PIPELINE_ID/runs/$RUN_ID?api-version=6.0-preview.1" | jq .
+
+```
---
+++ pkg/rest/rest.go
@@ -0,0 +1,111 @@
+package rest
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+type BasicAuth struct {
+	Username string
+	Password string
+}
+
+type URIParamaters struct {
+	Organization    string `json:"organization"`
+	PipelineID      int32  `json:"pipelineId"`
+	Project         string `json:"project"`
+	APIVersion      string `json:"api-version"`
+	PipelineVersion int32  `json:"pipelineVersion"`
+}
+
+type RequestBody struct {
+	PreviewRun bool `json:"previewRun,omitempty"`
+	Resources  struct {
+		Builds       string `json:"builds,omitempty"`
+		Containers   string `json:"containers,omitempty"`
+		Packages     string `json:"packages,omitempty"`
+		Pipelines    string `json:"pipelines,omitempty"`
+		Repositories struct {
+			Self struct {
+				RefName string `json:"refName"`
+			} `json:"self"`
+		} `json:"repositories"`
+	} `json:"resources"`
+	StagesToSkip       []string    `json:"stagesToSkip,omitempty"`
+	TemplateParameters interface{} `json:"templateParameters,omitempty"`
+	Variables          interface{} `json:"variables"`
+	YamlOverride       interface{} `json:"yamlOverride,omitempty"`
+}
+
+type Response struct {
+	Links struct {
+		Self struct {
+			Href string `json:"href"`
+		} `json:"self"`
+		Web struct {
+			Href string `json:"href"`
+		} `json:"web"`
+		PipelineWeb struct {
+			Href string `json:"href"`
+		} `json:"pipeline.web"`
+		Pipeline struct {
+			Href string `json:"href"`
+		} `json:"pipeline"`
+	} `json:"_links"`
+	Pipeline struct {
+		URL      string `json:"url"`
+		ID       int    `json:"id"`
+		Revision int    `json:"revision"`
+		Name     string `json:"name"`
+		Folder   string `json:"folder"`
+	} `json:"pipeline"`
+	State       string    `json:"state"`
+	Result      string    `json:"result"`
+	CreatedDate time.Time `json:"createdDate"`
+	URL         string    `json:"url"`
+	Resources   struct {
+		Repositories struct {
+			Self struct {
+				Repository struct {
+					ID   string `json:"id"`
+					Type string `json:"type"`
+				} `json:"repository"`
+				RefName string `json:"refName"`
+				Version string `json:"version"`
+			} `json:"self"`
+		} `json:"repositories"`
+	} `json:"resources"`
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func Call(url, method string, requestBody []byte, auth BasicAuth) ([]byte, error) {
+
+	var body []byte
+
+	client := &http.Client{
+		Timeout: time.Second * 10,
+	}
+
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(requestBody))
+	if err != nil {
+		return body, fmt.Errorf("Error: %s", err.Error())
+	}
+
+	req.SetBasicAuth(auth.Username, auth.Password)
+	req.Header.Add("Content-Type", "application/json")
+
+	response, err := client.Do(req)
+	if err != nil {
+		return body, fmt.Errorf("Error: %s", err.Error())
+	}
+
+	body, err = io.ReadAll(response.Body)
+
+	defer response.Body.Close()
+
+	return body, nil
+}
